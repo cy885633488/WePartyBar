4abce7568947561640034f904f9384d574b48bef
cy885633488 <yuanan.an@foxmail.com>
2016-06-02 16:19:00 +0800

    实现闪屏

--- src/cn/ucai/WePartyBar/activity/SplashActivity.java
+++ src/cn/ucai/WePartyBar/activity/SplashActivity.java
@@ -30,9 +30,9 @@ public class SplashActivity extends BaseActivity {
 		super.onCreate(arg0);
 
 		rootLayout = (RelativeLayout) findViewById(R.id.splash_root);
-		versionText = (TextView) findViewById(R.id.tv_version);
+//		versionText = (TextView) findViewById(R.id.tv_version);
 
-		versionText.setText(getVersion());
+//		versionText.setText(getVersion());
 		AlphaAnimation animation = new AlphaAnimation(0.3f, 1.0f);
 		animation.setDuration(1500);
 		rootLayout.startAnimation(animation);
